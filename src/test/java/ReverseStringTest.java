ca03b0257bd5a7367decc3a55ecf85d41be52d59
Borche-1 <borche.naumoski@amio.io>
2022-03-18 12:14:42 +0100

    Unit Test Assertions

--- src/test/java/ReverseStringTest.java
+++ src/test/java/ReverseStringTest.java
@@ -2,20 +2,11 @@ import Reverse.ReverseString;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 public class ReverseStringTest {
-
-    @BeforeClass
-
-    @BeforeMethod
 
     @Test
     void verifyTheTestForReversText(){
-          //new assertion
-        Assert.assertNotEquals("Programming is not easy if you don't know how to swim...", "...miws ot woh wonk t'nod uoy fi ysae ton si gnimmargorP");
+        Assert.assertEquals(ReverseString.reverseText( "Programming is not easy if you don't know how to swim..."), "...miws ot woh wonk t'nod uoy fi ysae ton si gnimmargorP");
     }
 
 
